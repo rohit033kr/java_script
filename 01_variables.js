31fa38fe49cb146f828e10517454402a90e8e5b8
Rohit <kumarrohitjamui305@gmail.com>
2026-02-21 21:25:19 +0530

    variables and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1931939
+let accountEmail = "rohit@google.com"
+var accountPassword = "owir92385"
+accountCity = "Jamui"
+let accountState; // return undefined
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/ 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+4)   //we are using node.js, not browser
+
+// code readability should be high..
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => the value is not defined
+// symbol => unique
+
+
+// object
+
+// console.log(typeof "rohit")
+// console.log(typeof null)  // ---> object
+console.log(typeof undefined) // ---> undefined
+
+
+
+
+
